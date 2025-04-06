5fdbea0d5bcdffc0c1477678b042071497da4f9a
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-04-06 13:41:30 +0700

    update login and product

--- login.ts
+++ login.ts
@@ -3,3 +3,6 @@ console.log("custom form");
 
 console.log("fitur login");
 console.log("custom form");
+
+console.log("fitur login");
+console.log("custom form");
--- product.ts
+++ product.ts
@@ -7,3 +7,8 @@ console.log("add product");
 console.log("feat: add product card");
 console.log("feat: add product page");
 console.log("update product");
+
+console.log("add product");
+console.log("feat: add product card");
+console.log("feat: add product page");
+console.log("update product");
