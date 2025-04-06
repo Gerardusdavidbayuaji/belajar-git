0ab50a896ddcbe586c1b95b0fa90bd9565bc2a59
Gerardusdavid <142146086+Gerardusdavidbayuaji@users.noreply.github.com>
2025-04-06 13:46:46 +0700

    update product

--- product.ts
+++ product.ts
@@ -7,3 +7,13 @@ console.log("add product");
 console.log("feat: add product card");
 console.log("feat: add product page");
 console.log("update product");
+
+console.log("add product");
+console.log("feat: add product card");
+console.log("feat: add product page");
+console.log("update product");
+
+console.log("add product");
+console.log("feat: add product card");
+console.log("feat: add product page");
+console.log("update product");
